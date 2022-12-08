ceb92d9c3707f0a671c755dfe32c81e6d8cb4f4f
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-12-08 11:39:57 +0500

    add component search, finish video 6

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,35 +1,36 @@
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
+import BlogFilter from "../components/BlogFilter";
 
 const BlogPage = () => {
   const [posts, setPosts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const postQuery = searchParams.get("post") || "";
+  let latest = searchParams.has("latest");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const query = form.search.value;
-    setSearchParams({ post: query });
-  };
+  const startsFrom = latest ? 80 : 1;
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, []);
+
   return (
     <div>
       <h1>BlogPage</h1>
       <h2>Our News</h2>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <input type="search" name="search" />
-        <input type="submit" value="Search" />
-      </form>
+      <BlogFilter
+        postQuery={postQuery}
+        latest={latest}
+        setSearchParams={setSearchParams}
+      />
       <Link to="/posts/new">Add new post</Link>
       {posts
-        .filter((post) => post.title.includes(postQuery))
+        .filter(
+          (post) => post.title.includes(postQuery) && post.id >= startsFrom
+        )
         .map((post) => (
           <Link key={post.id} to={`/posts/${post.id}`}>
             <li>{post.title}</li>
