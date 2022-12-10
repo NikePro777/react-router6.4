3fdeaf1aaeaa8a364061365145fc4afc36734826
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-12-10 08:15:36 +0500

    add post and comments loader, data get in with hook useAsyncValue, finish video 8

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,7 +1,41 @@
-import { Link, useLoaderData, useNavigate } from "react-router-dom";
+import { Suspense } from "react";
+import {
+  Await,
+  defer,
+  Link,
+  useAsyncValue,
+  useLoaderData,
+  useNavigate,
+} from "react-router-dom";
+
+const Post = () => {
+  const post = useAsyncValue();
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
+};
+
+const Comments = () => {
+  const comments = useAsyncValue();
+  return (
+    <div>
+      <h2>Comments</h2>
+      {comments.map((comment) => (
+        <>
+          <h3>{comment.email}</h3>
+          <h4>{comment.name}</h4>
+          <p>{comment.body}</p>
+        </>
+      ))}
+    </div>
+  );
+};
 
 const SinglePage = () => {
-  const { post, id } = useLoaderData();
+  const { post, id, comments } = useLoaderData();
   const navigate = useNavigate();
 
   const goBack = () => navigate(-1);
@@ -9,17 +43,42 @@ const SinglePage = () => {
   return (
     <div>
       <button onClick={goBack}>Назад</button>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      <Suspense fallback={<h2>Post is loading...</h2>}>
+        <Await resolve={post}>
+          <Post />
+        </Await>
+      </Suspense>
+      <Suspense fallback={<h2>Comments is loading...</h2>}>
+        <Await resolve={comments}>
+          <Comments />
+        </Await>
+      </Suspense>
       <Link to={`/posts/${id}/edit`}>Edit this posts</Link>
     </div>
   );
 };
+async function getPostId(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  return res.json();
+}
+
+async function getCommentsByPost(id) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+  );
+  return res.json();
+}
 
 export const postLoader = async ({ params }) => {
   const id = params.id;
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  const post = await res.json();
-  return { post, id };
+
+  return defer({
+    post: await getPostId(id),
+    id,
+    comments: getCommentsByPost(id),
+  });
+  {
+    /* post: await getPostId(id) - чтобы сначала грузились посты, а потом уже комментарии (внутри загрузим)*/
+  }
 };
 export default SinglePage;
